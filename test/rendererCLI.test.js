d9bd3e57cc85f8b6f4917f24b64ea6aebadae036
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-19 15:02:38 +0100

    refactor(tests): test improvements: - better (re) use of context variables - make sure each test can be run in parallel (different output file names) - various minor tweaks

--- test/rendererCLI.test.js
+++ test/rendererCLI.test.js
@@ -7,30 +7,26 @@ function existsSync (uri) {
   try {
     fs.statSync(uri)
     return true
-  }catch (error) {
+  } catch (error) {
     return false
   }
 }
 
-function rmSync (uri) {
-  try {
-    fs.unlinkSync(uri)
-  }catch (error) {
-  }
-}
-
 test.afterEach.always(t => {
-    // this runs after each test and other test hooks, even if they failed
-    //remove created file
-    try{
-      fs.unlinkSync(t.context.outputPath)
-    }catch(err){}
+  // this runs after each test and other test hooks, even if they failed
+  // remove created file
+  try {
+    console.log('removing file', t.context.outputPath)
+    fs.unlinkSync(t.context.outputPath)
+  // rmSync(outputPath)
+  } catch(err) {}
 })
 
 test.beforeEach(t => {
-  let jamPath = '../dist/rendererCLI.js'
+  let jamPath = '../dist/index.js'
   t.context = {
-    jamPath : path.resolve(__dirname, jamPath)
+    jamPath: path.resolve(__dirname, jamPath),
+    resolution: '160x120'
   }
 })
 
@@ -39,11 +35,11 @@ test('server side renderer: can take a path to a file as input, generate a rende
   // see https://github.com/stackgl/headless-gl/blob/master/.travis.yml
   const jamPath = t.context.jamPath
   const inputPath = path.resolve(__dirname, './data/cube.stl')
-  const outputPath = path.resolve(__dirname, './test.png')
+  const outputPath = path.resolve(__dirname, './test.stl.png')
   const expImagePath = path.resolve(__dirname, './data/exp.cube.stl.png')
-  const resolution = '160x120'
+  t.context = Object.assign({}, t.context, {outputPath})
 
-  const cmd = `node ${jamPath} ${inputPath} ${resolution} ${outputPath} `
+  const cmd = `babel-node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
   require('child_process').execSync(cmd, {stdio: [0, 1, 2]})
   // t.deepEqual(meshSource[0], 'fakeModel.stl')
   t.deepEqual(true, existsSync(outputPath))
@@ -55,29 +51,19 @@ test('server side renderer: can take a path to a file as input, generate a rende
       const dist = Jimp.distance(exp, obs)
       const identical = (dist < 0.15 && diff.percent < 0.15)
       t.deepEqual(true, identical)
-      rmSync(outputPath)
     }).catch(function () {
-      rmSync(outputPath)
-      t.fail('Files are not identical', expImagePath, outputPath)
-    })
+    t.fail('Files are not identical', expImagePath, outputPath)
+  })
 })
 
-/*
 test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(3mf)', t => {
-  let jamPath = './rendererCLI.js'
-  let inputPath = './data/cube_gears.3mf'// cube_gears.3mf'// dodeca_chain_loop_color.3mf'// pyramid_vertexcolor.3mf'
-  let outputPath = './test.png'
-  let resolution = '160x120'
+  const jamPath = t.context.jamPath
+  const inputPath = path.resolve(__dirname, './data/cube_gears.3mf')// cube_gears.3mf'// dodeca_chain_loop_color.3mf'// pyramid_vertexcolor.3mf'
+  const outputPath = path.resolve(__dirname, './test.3mf.png')
+  const expImagePath = path.resolve(__dirname, './data/exp.cube_gears.3mf.png')
+  t.context = Object.assign({}, t.context, {outputPath})
 
-  let expImagePath = './data/exp.cube_gears.3mf.png'
-
-  jamPath = path.resolve(__dirname, jamPath)
-  inputPath = path.resolve(__dirname, inputPath)
-  outputPath = path.resolve(__dirname, outputPath)
-  outputPath = path.resolve(outputPath)
-  expImagePath = path.resolve(__dirname, expImagePath)
-
-  const cmd = `babel-node ${jamPath} ${inputPath} ${resolution} ${outputPath} `
+  const cmd = `babel-node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
   require('child_process').execSync(cmd, {stdio: [0, 1, 2]})
   t.deepEqual(true, existsSync(outputPath))
 
@@ -95,21 +81,14 @@ test('server side renderer: can take a path to a file as input, generate a rende
     })
 })
 
-test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(obj)', function () {
-  let jamPath = './rendererCLI.js'
-  let inputPath = './data/cube.obj'
-  let outputPath = './test.png'
-  let resolution = '160x120'
+test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(obj)', t => {
+  const jamPath = t.context.jamPath
+  const inputPath = path.resolve(__dirname, './data/cube.obj')
+  const outputPath = path.resolve(__dirname, './test.obj.png')
+  const expImagePath = path.resolve(__dirname, './data/exp.cube.obj.png')
+  t.context = Object.assign({}, t.context, {outputPath})
 
-  let expImagePath = './data/exp.cube.obj.png'
-
-  jamPath = path.resolve(__dirname, jamPath)
-  inputPath = path.resolve(__dirname, inputPath)
-  outputPath = path.resolve(__dirname, outputPath)
-  outputPath = path.resolve(outputPath)
-  expImagePath = path.resolve(__dirname, expImagePath)
-
-  const cmd = `babel-node ${jamPath} ${inputPath} ${resolution} ${outputPath} `
+  const cmd = `babel-node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
   require('child_process').execSync(cmd, {stdio: [0, 1, 2]})
 
   t.deepEqual(true, existsSync(outputPath))
@@ -128,21 +107,14 @@ test('server side renderer: can take a path to a file as input, generate a rende
     })
 })
 
-test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(ctm)', function () {
-  let jamPath = './rendererCLI.js'
-  let inputPath = './data/LeePerry.ctm'
-  let outputPath = './test.png'
-  let resolution = '160x120'
+test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(ctm)', t => {
+  const jamPath = t.context.jamPath
+  const inputPath = path.resolve(__dirname, './data/LeePerry.ctm')
+  const outputPath = path.resolve(__dirname, './test.ctm.png')
+  const expImagePath = path.resolve(__dirname, './data/exp.LeePerry.ctm.png')
+  t.context = Object.assign({}, t.context, {outputPath})
 
-  let expImagePath = './data/exp.LeePerry.ctm.png'
-
-  jamPath = path.resolve(__dirname, jamPath)
-  inputPath = path.resolve(__dirname, inputPath)
-  outputPath = path.resolve(__dirname, outputPath)
-  outputPath = path.resolve(outputPath)
-  expImagePath = path.resolve(__dirname, expImagePath)
-
-  const cmd = `babel-node ${jamPath} ${inputPath} ${resolution} ${outputPath} `
+  const cmd = `babel-node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
   require('child_process').execSync(cmd, {stdio: [0, 1, 2]})
 
   t.deepEqual(true, existsSync(outputPath))
@@ -159,4 +131,4 @@ test('server side renderer: can take a path to a file as input, generate a rende
       rmSync(outputPath)
       t.fail('Files are not identical', expImagePath, outputPath)
     })
-})*/
+})
