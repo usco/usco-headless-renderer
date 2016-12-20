4a40a185eaec9ea0a2b509212b7c18dafc008c77
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-20 14:33:35 +0100

    refactor(test): tweaked tests

--- test/rendererCLI.test.js
+++ test/rendererCLI.test.js
@@ -16,9 +16,7 @@ test.afterEach.always(t => {
   // this runs after each test and other test hooks, even if they failed
   // remove created file
   try {
-    console.log('removing file', t.context.outputPath)
     fs.unlinkSync(t.context.outputPath)
-  // rmSync(outputPath)
   } catch(err) {}
 })
 
@@ -30,6 +28,8 @@ test.beforeEach(t => {
   }
 })
 
+// note : use this with the node xxx command to debug in chrome: --inspect --debug-brk
+
 test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(stl)', t => {
   // FIXME: can only be run locally (webgl support needed), commented out for now for travisCI
   // see https://github.com/stackgl/headless-gl/blob/master/.travis.yml
@@ -39,7 +39,7 @@ test('server side renderer: can take a path to a file as input, generate a rende
   const expImagePath = path.resolve(__dirname, './data/exp.cube.stl.png')
   t.context = Object.assign({}, t.context, {outputPath})
 
-  const cmd = `babel-node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
+  const cmd = `node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
   require('child_process').execSync(cmd, {stdio: [0, 1, 2]})
   // t.deepEqual(meshSource[0], 'fakeModel.stl')
   t.deepEqual(true, existsSync(outputPath))
@@ -49,7 +49,7 @@ test('server side renderer: can take a path to a file as input, generate a rende
       const [exp, obs] = values
       const diff = Jimp.diff(exp, obs)
       const dist = Jimp.distance(exp, obs)
-      const identical = (dist < 0.15 && diff.percent < 0.15)
+      const identical = (dist < 0.1 && diff.percent < 0.1)
       t.deepEqual(true, identical)
     }).catch(function () {
     t.fail('Files are not identical', expImagePath, outputPath)
@@ -58,12 +58,12 @@ test('server side renderer: can take a path to a file as input, generate a rende
 
 test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(3mf)', t => {
   const jamPath = t.context.jamPath
-  const inputPath = path.resolve(__dirname, './data/cube_gears.3mf')// cube_gears.3mf'// dodeca_chain_loop_color.3mf'// pyramid_vertexcolor.3mf'
+  const inputPath = path.resolve(__dirname, './data/cube_gears.3mf') // cube_gears.3mf'// dodeca_chain_loop_color.3mf'// pyramid_vertexcolor.3mf'
   const outputPath = path.resolve(__dirname, './test.3mf.png')
   const expImagePath = path.resolve(__dirname, './data/exp.cube_gears.3mf.png')
   t.context = Object.assign({}, t.context, {outputPath})
 
-  const cmd = `babel-node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
+  const cmd = `node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
   require('child_process').execSync(cmd, {stdio: [0, 1, 2]})
   t.deepEqual(true, existsSync(outputPath))
 
@@ -72,13 +72,11 @@ test('server side renderer: can take a path to a file as input, generate a rende
       let [exp, obs] = values
       let diff = Jimp.diff(exp, obs)
       let dist = Jimp.distance(exp, obs)
-      const identical = (dist < 0.15 && diff.percent < 0.15)
+      const identical = (dist < 0.1 && diff.percent < 0.1)
       t.deepEqual(true, identical)
-      rmSync(outputPath)
     }).catch(function () {
-      rmSync(outputPath)
-      t.fail('Files are not identical', expImagePath, outputPath)
-    })
+    t.fail('Files are not identical', expImagePath, outputPath)
+  })
 })
 
 test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(obj)', t => {
@@ -88,7 +86,7 @@ test('server side renderer: can take a path to a file as input, generate a rende
   const expImagePath = path.resolve(__dirname, './data/exp.cube.obj.png')
   t.context = Object.assign({}, t.context, {outputPath})
 
-  const cmd = `babel-node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
+  const cmd = `node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
   require('child_process').execSync(cmd, {stdio: [0, 1, 2]})
 
   t.deepEqual(true, existsSync(outputPath))
@@ -98,13 +96,11 @@ test('server side renderer: can take a path to a file as input, generate a rende
       let [exp, obs] = values
       let diff = Jimp.diff(exp, obs)
       let dist = Jimp.distance(exp, obs)
-      const identical = (dist < 0.15 && diff.percent < 0.15)
+      const identical = (dist < 0.1 && diff.percent < 0.1)
       t.deepEqual(true, identical)
-      rmSync(outputPath)
     }).catch(function () {
-      rmSync(outputPath)
-      t.fail('Files are not identical', expImagePath, outputPath)
-    })
+    t.fail('Files are not identical', expImagePath, outputPath)
+  })
 })
 
 test('server side renderer: can take a path to a file as input, generate a render of that 3d file as ouput(ctm)', t => {
@@ -114,7 +110,7 @@ test('server side renderer: can take a path to a file as input, generate a rende
   const expImagePath = path.resolve(__dirname, './data/exp.LeePerry.ctm.png')
   t.context = Object.assign({}, t.context, {outputPath})
 
-  const cmd = `babel-node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
+  const cmd = `node ${jamPath} ${inputPath} ${t.context.resolution} ${outputPath} `
   require('child_process').execSync(cmd, {stdio: [0, 1, 2]})
 
   t.deepEqual(true, existsSync(outputPath))
@@ -124,11 +120,9 @@ test('server side renderer: can take a path to a file as input, generate a rende
       let [exp, obs] = values
       let diff = Jimp.diff(exp, obs)
       let dist = Jimp.distance(exp, obs)
-      const identical = (dist < 0.15 && diff.percent < 0.15)
+      const identical = (dist < 0.1 && diff.percent < 0.1)
       t.deepEqual(true, identical)
-      rmSync(outputPath)
     }).catch(function () {
-      rmSync(outputPath)
-      t.fail('Files are not identical', expImagePath, outputPath)
-    })
+    t.fail('Files are not identical', expImagePath, outputPath)
+  })
 })
