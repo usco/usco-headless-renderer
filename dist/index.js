435d2d551a23402b7c90f95f3527eb8f44fc9866
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-01-26 10:44:58 +0100

    chore(build): added missing shebang etc

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 'use strict';
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
@@ -151,7 +152,7 @@ if (args.length > 0) {
       .pipe(parsers[ext]) // we get a stream back
       .on('data', function (geometry) {
         console.log('done with parsing') // , parsedData)
-    
+
         render({entities: [], camera: {projection: null}, view: null, background: [1, 1, 1, 1]})
         writeContextToFile(gl, 256, 256, 4, outputPath)
       })*/
